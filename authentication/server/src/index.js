b8667f3e409e7290724244cf368f0dc6cb71959c
Christian Balderrama <0christianbalderrama@gmail.com>
2021-06-05 18:55:09 +0800

    :sparkles: Changes

---
+++ authentication/server/src/index.js
@@ -0,0 +1,15 @@
+import "regenerator-runtime";
+import express from "express";
+import morgan from "morgan";
+
+const port = process.env.PORT || 3000;
+const env = process.env.NODE_ENV;
+const app = express();
+
+app.use(morgan("dev"));
+
+app.get("/", async(_, res) => {
+  return res.send("Hello, world!");
+});
+
+app.listen(port, () => `Server is now listening to port: ${port}`);
