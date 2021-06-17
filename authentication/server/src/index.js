1ad0b0d46a1069d7f3ee766239090f11d5b598ad
Christian Balderrama <0christianbalderrama@gmail.com>
2021-06-18 02:30:58 +0800

    :sparkles: Initial setup for client and backend

--- authentication/server/src/index.js
+++ authentication/server/src/index.js
@@ -1,11 +1,13 @@
 import "regenerator-runtime";
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV;
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 
 app.get("/", async(_, res) => {
